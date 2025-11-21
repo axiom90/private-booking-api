179efefaad08435ee162426f6eaa89bf68f8bef1
axiom <axiom90@proton.me>
2025-11-21 17:36:23 +0300

    refactor: apply caching on db client

--- src/service/db.py
+++ src/service/db.py
@@ -1,17 +1,12 @@
-# db.py
-from supabase import acreate_client, create_client, Client, AsyncClient
+from functools import lru_cache
+from supabase import (
+    acreate_client, create_client, Client, AsyncClient
+)
 from .config import config
 
-
+@lru_cache(maxsize=1)
 def get_supabase_client() -> Client:
     return create_client(
-        config.SUPABASE_URL, 
-        config.SUPABASE_KEY
-    )
-
-
-async def get_supabase_async_client() -> AsyncClient:
-    return await acreate_client(
         config.SUPABASE_URL, 
         config.SUPABASE_KEY
     )
